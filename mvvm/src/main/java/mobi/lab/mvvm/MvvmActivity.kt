6b90e3eb78d6268c85fe2b46a7bf24653373b0e6
lauris <lauris.kruusamae@lab.mobi>
2020-05-29 15:33:48 +0300

    Improve lazyViewModel function comment

--- mvvm/src/main/java/mobi/lab/mvvm/MvvmActivity.kt
+++ mvvm/src/main/java/mobi/lab/mvvm/MvvmActivity.kt
@@ -17,8 +17,9 @@ abstract class MvvmActivity : AppCompatActivity, MvvmLiveDataExtensions {
      * A wrapper function for Kotlin's lazy init.
      * inline with a reified type T so that we could pass T::class reference to ViewModel creation.
      *
-     * The factory argument is a function returning a ViewModelProcider.Factory. The noinline keyword means that this
-     * parameters will not be inlined. This results in the following syntax:
+     * The factory argument is a function returning a ViewModelProvider.Factory. The noinline keyword means that the
+     * parameter will not be inlined resulting in the following syntax:
+     *
      * |    MyViewModel as T     |  lazy wrapper  | factory lambda written outside of the function |
      * val viewModel: MyViewModel by lazyViewModel { ViewModelFactory() }
      */
